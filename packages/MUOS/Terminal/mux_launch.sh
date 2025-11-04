b235e9507c1cd4778c75256495a97e7af2976f57
Stanley00 <1281134+Stanley00@users.noreply.github.com>
2025-11-04 16:28:55 +0000

    Update mux_launch.sh script

--- packages/MUOS/Terminal/mux_launch.sh
+++ packages/MUOS/Terminal/mux_launch.sh
@@ -7,22 +7,16 @@
 
 echo app >/tmp/act_go
 
+GOV_GO="/tmp/gov_go"
+[ -e "$GOV_GO" ] && cat "$GOV_GO" >"$(GET_VAR "device" "cpu/governor")"
+
 SETUP_SDL_ENVIRONMENT
 
-BASE_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/Terminal"
+BASE_DIR="$1"
 cd "$BASE_DIR" || exit
 
 SET_VAR "system" "foreground_process" "terminal"
 
-(
-        while ! pgrep -f "terminal" >/dev/null; do
-                sleep 0.25
-        done
-        sleep 1
-        evemu-event "$(GET_VAR "device" "input/ev1")" --type "$(GET_VAR "device" "input/type/dpad/right")" --code "$(GET_VAR "device" "input/code/dpad/right")" --value 1
-        evemu-event "$(GET_VAR "device" "input/ev1")" --type "$(GET_VAR "device" "input/type/dpad/left")" --code "$(GET_VAR "device" "input/code/dpad/left")" --value -1
-) &
+./terminal -b res/background.png  --key-set -res/bash.keys --key-set -res/nano.keys --key-set -res/vim.keys --key-set -res/shell.keys --key-set +res/ctrl-shell.keys --key-set -res/vaixterm.keys --osk-layout res/qwerty.kb 
 
-SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "muOS-Keys" "/usr/lib/gamecontrollerdb.txt") ./terminal -b res/background.png  --key-set -res/bash.keys --key-set -res/nano.keys --key-set -res/vim.keys --key-set -res/shell.keys --key-set +res/ctrl-shell.keys --key-set -res/vaixterm.keys --osk-layout res/qwerty.kb 
-
-unset SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
+unset SDL_ASSERT SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
